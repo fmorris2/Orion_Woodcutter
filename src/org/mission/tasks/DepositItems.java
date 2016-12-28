2a4c7f1dba57c269a38787bbb652a6545679e5d6
Dad <Dad>
2016-12-28 08:04:08 -0500

    Minor change

--- src/org/mission/tasks/DepositItems.java
+++ src/org/mission/tasks/DepositItems.java
@@ -1,16 +1,17 @@
 package org.mission.tasks;
 
+import org.mission.OrionWoodcutter;
+
 import viking.api.Timing;
 import viking.api.skills.woodcutting.enums.AxeType;
-import viking.framework.mission.Mission;
 import viking.framework.task.Task;
 
 /**
  * Created by Sphiinx on 12/27/2016.
  */
-public class DepositItems extends Task {
+public class DepositItems extends Task<OrionWoodcutter> {
 
-    public DepositItems(Mission mission) {
+    public DepositItems(OrionWoodcutter mission) {
         super(mission);
     }
 
@@ -24,12 +25,8 @@ public class DepositItems extends Task {
                 Timing.waitCondition(() -> !inventory.isFull(), 150, random(2000, 2500));
         } else {
             if (bankUtils.isInBank()) {
-                try {
-                    if (bank.open())
-                        Timing.waitCondition(conditions.BANK_OPEN, 150, random(2000, 2500));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                if (bankUtils.open())
+                    Timing.waitCondition(conditions.BANK_OPEN, 150, random(2000, 2500));
             } else {
                 if (walkUtils.walkToArea(bankUtils.getClosest()))
                     Timing.waitCondition(() -> bankUtils.isInBank(), 150, random(2000, 2500));
